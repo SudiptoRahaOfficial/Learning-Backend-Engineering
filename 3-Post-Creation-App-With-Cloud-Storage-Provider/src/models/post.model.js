e0978b7d128c55f4ce956f180feca48067880223
Sudipto Raha Official <sudiptoraha.official@gmail.com>
2026-09-04 01:15:07 +0600

    maked post schema & model

---
+++ 3-Post-Creation-App-With-Cloud-Storage-Provider/src/models/post.model.js
@@ -0,0 +1,23 @@
+// importing dependencis
+const { Schema, model } = require('mongoose')
+
+// making schema
+const postSchema = new Schema(
+	{
+		image: {
+			type: String,
+			required: true,
+		},
+		caption: {
+			type: String,
+			required: true,
+		},
+	},
+	{ timestamps: true },
+)
+
+// making model
+const postModel = model('note', postSchema)
+
+// exporting model
+module.exports = postModel
