a077e6e5088d95cf7642f1e67ab2703291ec5436
Sudipto Raha Official <sudiptoraha.official@gmail.com>
2026-09-04 20:06:10 +0600

    maked - post create api

--- 3-Post-Creation-App-With-Cloud-Storage-Provider/src/app.js
+++ 3-Post-Creation-App-With-Cloud-Storage-Provider/src/app.js
@@ -1,6 +1,8 @@
 // importing dependencis
 const express = require('express')
 const multer = require('multer')
+const uploadFile = require('./services/storage.service')
+const postModel = require('./models/post.model')
 
 // making app
 const app = express()
@@ -17,7 +19,20 @@ const upload = multer({ storage: multer.memoryStorage() })
 
 // create post
 app.post('/create-post', upload.single('image'), async (req, res) => {
-	
+	// uploading file-buffer to imagekit
+	const result = await uploadFile(req.file.buffer)
+
+	// creating post
+	const post = await postModel.create({
+		image: result.url,
+		caption: req.body.caption,
+	})
+
+	// response back
+	return res.status(201).json({
+		message: 'post created successfully',
+		post,
+	})
 })
 
 // exporting app
