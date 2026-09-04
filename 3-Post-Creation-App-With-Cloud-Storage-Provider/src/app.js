3a1dbe874134e0324efb22a1265c43deca13c853
Sudipto Raha Official <sudiptoraha.official@gmail.com>
2026-09-04 20:30:13 +0600

    maked - all posts fetching api

--- 3-Post-Creation-App-With-Cloud-Storage-Provider/src/app.js
+++ 3-Post-Creation-App-With-Cloud-Storage-Provider/src/app.js
@@ -17,22 +17,44 @@ app.use(middlewares) // using middlewares
 // image uploading middleware
 const upload = multer({ storage: multer.memoryStorage() })
 
-// create post
+// create post api
 app.post('/create-post', upload.single('image'), async (req, res) => {
-	// uploading file-buffer to imagekit
-	const result = await uploadFile(req.file.buffer)
+	try {
+		// uploading file-buffer to imagekit
+		const result = await uploadFile(req.file.buffer)
 
-	// creating post
-	const post = await postModel.create({
-		image: result.url,
-		caption: req.body.caption,
-	})
+		// creating post
+		const post = await postModel.create({
+			image: result.url,
+			caption: req.body.caption,
+		})
 
-	// response back
-	return res.status(201).json({
-		message: 'post created successfully',
-		post,
-	})
+		// response back
+		return res.status(201).json({
+			message: 'post created successfully',
+			post,
+		})
+	} catch (error) {
+		console.log(error)
+		return res.status(500).json({ message: 'server error' })
+	}
+})
+
+// fetch all posts api
+app.get('/posts', async (req, res) => {
+	try {
+		// fetching all db stored posts
+		const posts = await postModel.find()
+
+		// response back
+		return res.status(200).json({
+			message: 'posts fetched successfully',
+			posts,
+		})
+	} catch (error) {
+		console.log(error)
+		return res.status(500).json({ message: 'server error' })
+	}
 })
 
 // exporting app
