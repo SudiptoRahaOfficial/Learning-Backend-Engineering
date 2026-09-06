9523d58741911e3caf4daa37542e5fd913b1a397
Sudipto Raha Official <sudiptoraha.official@gmail.com>
2026-09-06 23:45:08 +0600

    maked signin api

--- 5-advanced-authentication/src/controllers/auth.controllers.js
+++ 5-advanced-authentication/src/controllers/auth.controllers.js
@@ -56,7 +56,60 @@ async function signupPostController(req, res) {
 	}
 }
 
+// controller for signin post route
+async function signinPostController(req, res) {
+	// extracting all data sent by client
+	const { username, email, password } = req.body
+
+	try {
+		// finding user to db by username & email both
+		const user = await userModel.findOne({
+			$or: [{ username }, { email }],
+		})
+
+		// throwing error if user not found by username/email both
+		if (!user) {
+			return res.status(401).json({ message: 'Invalid credentials' })
+		}
+
+		// checking is provided password valid/invalid
+		const isPasswordValid = await bcrypt.compare(password, user.password)
+
+		// throwing error if password got invalid
+		if (!isPasswordValid) {
+			return res.status(401).json({ message: 'Invalid credentials' })
+		}
+
+		// generating token
+		const token = jwt.sign(
+			{
+				id: user._id,
+				role: user.role,
+			},
+			process.env.JWT_SECRET,
+		)
+		// setting token to user's browser cookie
+		res.cookie('token', token)
+
+		// response back on success
+		return res.status(200).json({
+			message: 'User signed in successfully',
+			user: {
+				id: user._id,
+				username: user.username,
+				email: user.email,
+				role: user.role,
+			},
+		})
+	} catch (error) {
+		console.log(error)
+		// response back on error
+		return res.status(500).json({ message: 'Server error' })
+	}
+}
+
 // exporting controllers
 module.exports = {
 	signupPostController,
+	signinPostController,
 }
--- 5-advanced-authentication/src/routes/auth.routes.js
+++ 5-advanced-authentication/src/routes/auth.routes.js
@@ -1,11 +1,13 @@
 // importing dependencis
 const router = require('express').Router()
 const {
-    signupPostController
+	signupPostController,
+	signinPostController,
 } = require('../controllers/auth.controllers')
 
 // signup post api
 router.post('/signup', signupPostController)
+router.post('/signin', signinPostController)
 
 // exporting router
 module.exports = router
