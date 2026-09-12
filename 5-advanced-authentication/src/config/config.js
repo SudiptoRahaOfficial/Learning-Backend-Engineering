c56ec08dbee7d5aa3cf97ded41c53d9f5c70b41e
Sudipto Raha Official <sudiptoraha.official@gmail.com>
2026-09-12 15:10:16 +0600

    added google oAuth credentials at config.js file & .env file

--- 5-advanced-authentication/src/config/config.js
+++ 5-advanced-authentication/src/config/config.js
@@ -14,6 +14,18 @@ if (!process.env.JWT_SECRET) {
 if (!process.env.IMAGEKIT_PRIVATE_KEY) {
 	throw new Error('IMAGEKIT_PRIVATE_KEY is not defined in .env')
 }
+if (!process.env.GOOGLE_CLIENT_ID) {
+	throw new Error('GOOGLE_CLIENT_ID is not defined in .env')
+}
+if (!process.env.GOOGLE_CLIENT_SECRET) {
+	throw new Error('GOOGLE_CLIENT_SECRET is not defined in .env')
+}
+if (!process.env.GOOGLE_REFRESH_TOKEN) {
+	throw new Error('GOOGLE_REFRESH_TOKEN is not defined in .env')
+}
+if (!process.env.GOOGLE_USER) {
+	throw new Error('GOOGLE_USER is not defined in .env')
+}
 
 // configuration object
 const config = {
@@ -21,6 +33,10 @@ const config = {
 	DB_CONNECTION_URI: process.env.DB_CONNECTION_URI,
 	JWT_SECRET: process.env.JWT_SECRET,
 	IMAGEKIT_PRIVATE_KEY: process.env.IMAGEKIT_PRIVATE_KEY,
+	GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+	GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+	GOOGLE_REFRESH_TOKEN: process.env.GOOGLE_REFRESH_TOKEN,
+	GOOGLE_USER: process.env.GOOGLE_USER,
 }
 
 // exporting config object
